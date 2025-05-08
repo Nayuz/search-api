e4e3130d7820c9c3632ba465a66d38efdbb0117e
Nayuz <nayuz.nil@gmail.com>
2025-05-08 15:50:17 +0900

    잘못작성된 파일 삭제

--- app/core/config.py
+++
@@ -1,9 +0,0 @@
-from pydantic_settings import BaseSettings
-
-class Setting(BaseSettings):
-    PROJECT_NAME: str = "SearchAPI"
-
-    class Config:
-        env_file = ".env"
-
-setting = Setting()
